965893984d1a08ec01de59f21fad30ad04c04a58
Sean Martin <sean.martin@gmail.com>
2018-09-24 23:44:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  collection = []
+  if block_given?
+    i = 0
+    while i < array.length
+      collection.push(yield(array[i]))
+      i += 1
+    end
+  end
+  collection
+end
 
-
+p my_collect([1, 2, 3, 4,5]) { |num| num**2 }
