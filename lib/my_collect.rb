57a6cd110371ae3edcf6cd9b4c2f3adf1dfcb5f1
Sean Martin <sean.martin@gmail.com>
2018-09-24 23:42:34 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+    collection = []
 
+    while i < array.length
+      collection.push(yield(array[i]))
+      i += 1
+    end
+  end
+  collection
+end
 
